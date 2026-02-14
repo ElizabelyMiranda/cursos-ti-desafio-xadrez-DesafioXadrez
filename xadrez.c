a57ce061b1aa5442aa2851d5838d744a3f1a4bea
ElizabelyMiranda <bebelytounanet@hotmail.com>
2026-02-14 11:19:32 -0300

    Add initial print statements for chess game

--- xadrez.c
+++ xadrez.c
@@ -5,6 +5,8 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
+    printf("Desafio Jogo de Xadrez\n");
+    printf(''Novo commit(''Novo commit\n'');
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
